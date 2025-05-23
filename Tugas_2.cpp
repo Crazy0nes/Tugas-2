735a09324e4f779a677d935dce629f7f418fc8f8
Crazy0nes <ajax9370@gmail.com>
2025-05-23 23:05:21 +0700

    Revisi & Finalisasi

--- Tugas_2.cpp
+++ Tugas_2.cpp
@@ -1,121 +1,104 @@
 #include <iostream>
 using namespace std;
 
-struct Node
-{
-    string data;
-    Node* next;
-    Node* prev;
+struct Buku {
+    string judul;
+    string penulis;
+    Buku* prev;
+    Buku* next;
 };
 
-struct Node* buatnode(string data){
-    // Node* newNode = new Node{data, NULL, NULL};
-    Node* newNode = (Node*)malloc(sizeof(data));
-    newNode->data = data;
-    newNode->next = NULL;
-    newNode->prev= NULL;
-    return newNode;
+Buku* head = nullptr;
+Buku* tail = nullptr;
+
+Buku* buatNode(string judul, string penulis) {
+    Buku* baru = new Buku;
+    baru->judul = judul;
+    baru->penulis = penulis;
+    baru->next = nullptr;
+    baru->prev = nullptr;
+    return baru;
 }
 
-void sisipnodedepan(Node** head, string data){
-
-    Node* newNode = buatnode(data);
-    newNode->next = *head;
-    if(*head != NULL){
-
-        (*head)->prev = newNode;
-
+void Sisipnodedepan(string judul, string penulis) {
+    Buku* baru = buatNode(judul, penulis);
+    if (!head) {
+        head = tail = baru;
+    } else {
+        baru->next = head;
+        head->prev = baru;
+        head = baru;
     }
-    *head = newNode;
 }
 
-void sisipnodebelakang(Node** head, string data){
+void Sisipnodebelakang(string judul, string penulis) {
+    Buku* baru = buatNode(judul, penulis);
+    if (!tail) {
+        head = tail = baru;
+    } else {
+        tail->next = baru;
+        baru->prev = tail;
+        tail = baru;
+    }
+}
 
-    Node* newNode = buatnode(data);
-    if(*head == NULL){
-
-        *head = newNode;
+void printdaridepan() {
+    if (!head) {
+        cout << "Daftar buku kosong.\n";
         return;
-
     }
-    Node* bantu = *head;
-    while(bantu->next != NULL){
-        bantu = bantu->next;
+    Buku* temp = head;
+    while (temp) {
+        cout << "=> Judul: " << temp->judul << " || Penulis: " << temp->penulis << endl;
+        temp = temp->next;
     }
-    bantu->next = newNode;
-    newNode->prev = bantu;
-
 }
 
-Node* searching(Node* head, string key){
-
-    Node* bantu = head;
-    while(bantu != NULL){
-        if(bantu->data == key){
-            return bantu;
-        }
-        bantu = bantu->next;
+void hapusnodetengah(string judul) {
+    if (!head) {
+        cout << "Daftar buku kosong.\n";
+        return;
     }
-    return NULL;
-
-}
-void printdaridepan(Node* head){
-
-    if(head == NULL){
-        cout << "Node nya ga ada!" << endl;
+    Buku* temp = head;
+    while (temp && temp->judul != judul) {
+        temp = temp->next;
     }
-    Node* bantu = head;
-    while(bantu != NULL){
-        cout << bantu->data << " ";
-        bantu = bantu->next;
-    }
-    cout << endl;
-}
-
-
-void hapusnodetengah(Node** head, string key){
-    if(*head == NULL){
-        cout << "List Kosong" << endl;
+    if (!temp) {
+        cout << "Buku tidak ditemukan.\n";
         return;
     }
 
-    Node* bantu = *head;
-    while(bantu != NULL && bantu->data != key){
-        bantu = bantu ->next;
+    if (temp == head && temp == tail) {
+        head = tail = nullptr;
+    } else if (temp == head) {
+        head = head->next;
+        if (head) head->prev = nullptr;
+    } else if (temp == tail) {
+        tail = tail->prev;
+        if (tail) tail->next = nullptr;
+    } else {
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
     }
 
-    if(bantu == NULL){
-        cout << "Node ga ketemu!" << endl;
-        return;
-    }
-    
-    if(bantu->prev != NULL){
-        bantu->prev->next = bantu->next;
-    }else{
-        *head = bantu->next;
-    }
-    if(bantu->next != NULL){
-
-        bantu->next->prev = bantu->prev;
-
-    }
-    free(bantu);
-
+    delete temp;
 }
+
 
 
 int main(){
-    Node* head = NULL;
+    Buku* head = nullptr;
+    Buku* tail = nullptr;
     bool retry = true;
-    string judul;
+    string judul,penulis;
     while(retry == true){
+        system("cls");
 cout << "=== MENU MANAJEMEN DAFTAR BUKU ===" << endl;
 cout << "1. Tambah Buku Di Depan" << endl;
 cout << "2. Tambah Buku Di Belakang" << endl;
 cout << "3. Hapus Buku berdasarkan Judul" << endl;
 cout << "4. Tampilkan Daftar Buku" << endl;
 cout << "5. Keluar" << endl;
-cout << endl << endl;
 cout << "Pilih Menu: ";
 int pilihan;
 cin >> pilihan;
@@ -130,33 +113,48 @@ case 1:
     cout << "Masukkan Judul Buku: ";
     cin.ignore();
     getline(cin, judul);
-    sisipnodedepan(&head, judul);
+    cout << "Masukkan Penulis Buku: ";
+    getline(cin, penulis);
+    Sisipnodedepan(judul, penulis);
     cout << "Buku berhasil ditambahkan di depan" << endl;
+    retry = true;
+    system("pause");
     break; 
 
 case 2:
     cout << "Masukkan Judul Buku: ";
     cin.ignore();
     getline(cin, judul);
-    sisipnodebelakang(&head, judul);
+    cout << "Masukkan Penulis Buku: ";
+    getline(cin, penulis);
+    Sisipnodebelakang(judul, penulis);
     cout << "Buku berhasil ditambahkan di belakang" << endl;
+    retry = true;
+    system("pause");
     break; 
 
 case 3:
     cout << "Masukkan Judul Buku yang ingin dihapus: ";
     cin.ignore();
     getline(cin, judul);
-    hapusnodetengah(&head, judul);
+    hapusnodetengah(judul);
     cout << "Buku berhasil dihapus" << endl;
+    retry = true;
+    system("pause");
     break; 
 
 case 4:
-    /* code */
+cout << endl;
+    cout << "Daftar Buku: " << endl;
+    printdaridepan();
+    retry = true;
+    system("pause");
     break; 
 
 case 5:
     retry = false;
     cout << "Terima Kasih" << endl;
+    system("pause");
     break;
 
 default:
